09f395ab1b3633f6f5168043a3699ef5c3cb4e0c
Nicolas Valencia <nicolasvalenciamadrid@gmail.com>
2023-08-29 18:15:19 -0500

    se agrega jwt en server

--- reto2/proyecto/server/server.py
+++ reto2/proyecto/server/server.py
@@ -6,17 +6,10 @@ import datetime
 print("hola mundo")
 
 # Clave secreta para firmar el JWT (cambia esto por una clave segura en un entorno de producción)
-secret_key = "7RfPwod0py1AtGUBYDswSuNPD4nTpv2U"
+
 
 # Datos que deseas incluir en el JWT
-payload = {
-    "user_id": 123,
-    "username": "ejemplo",
-    "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # Token expira en 1 hora
-}
 
-# Generar el JWT
-token = jwt.encode(payload, secret_key, algorithm="HS256")
 
 print("JWT generado:")
 print(token)
@@ -27,12 +20,26 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         parsed_path = urllib.parse.urlparse(self.path)
         query = urllib.parse.parse_qs(parsed_path.query)
-        if 'usuario' in query:
+        if 'usuario' in query and 'correo' in query:
             usuario = query['usuario'][0]
+            correo = query['correo'][0]
+
+            secret_key = "7RfPwod0py1AtGUBYDswSuNPD4nTpv2U"
+
+            payload = {
+                "usuario": usuario,
+                "correo": correo,
+                "fecha": datetime.datetime.utcnow(),
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # Token expira en 1 hora
+            }
+
+            # Generar el JWT
+            token = jwt.encode(payload, secret_key, algorithm="HS256")
+
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(bytes(f'Hola {usuario}', 'utf-8'))
+            self.wfile.write(bytes(f'{jwt}', 'utf-8'))
         else:
             self.send_response(400)
             self.send_header('Content-type', 'text/html')
