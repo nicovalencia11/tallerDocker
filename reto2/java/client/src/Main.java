ed5dd7c381bdb72a9107b3878b5eb0e645948dbd
danieleduardc <daniele.puertah@uqvirtual.edu.co>
2023-08-30 00:49:45 -0500

    culminacion de reto 3-4 de jwt

--- reto2/java/client/src/Main.java
+++ reto2/java/client/src/Main.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class Main {
     public static void main(String[] args) {
         String usuario = System.getenv().get("USUARIO");
-        URL url;
-        try {
-            Thread.sleep(2000); // Pausa por 2 segundos
-        } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            url = new URL("http://server:80?usuario=" + usuario
-                    + "&correo=dobby@gmail.com");
-            URLConnection con = url.openConnection();
-            BufferedReader resultado = new BufferedReader(new java.io.InputStreamReader(con.getInputStream()));
-            System.out.println(resultado.readLine());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        String serverUrl = System.getenv().get("SERVER_URL"); // Obtener la URL del servidor desde la variable de entorno
+
+        if (usuario != null && serverUrl != null) {
+            try {
+                URL url = new URL("http://" + serverUrl + ":80?usuario=" + usuario
+                        + "&correo=dobby@gmail.com");
+                URLConnection con = url.openConnection();
+                BufferedReader resultado = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                System.out.println(resultado.readLine());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } else {
+            System.out.println("Falta definir USUARIO y SERVER_URL en las variables de entorno.");
         }
     }
 }
