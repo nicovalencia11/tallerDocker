4233c9266affb08a41234ff686d78c1e3eb4adee
Nicolas Valencia <nicolasvalenciamadrid@gmail.com>
2023-09-12 18:50:09 -0500

    avance desarrollo

--- tallerApi/autenticacion/src/main/java/com/example/autenticacion/controllers/autenticacionController.java
+++ tallerApi/autenticacion/src/main/java/com/example/autenticacion/controllers/autenticacionController.java
@@ -1,10 +1,10 @@
-package com.example.autenticacion.resource;
+package com.example.autenticacion.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/autenticacion")
-public class autenticacionResource {
-    
+@RequestMapping("/api/v1/autenticacion")
+public class autenticacionController {
+
 }
---
+++ tallerApi/autenticacion/src/main/java/com/example/autenticacion/entities/usuarioEntity.java
@@ -0,0 +1,4 @@
+package com.example.autenticacion.entities;
+
+public class usuarioEntity {
+}
