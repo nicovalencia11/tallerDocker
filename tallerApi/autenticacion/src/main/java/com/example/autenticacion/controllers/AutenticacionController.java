8bf7e09b167b1e93eacb90437fa566154dfb0cec
danieleduardc <daniele.puertah@uqvirtual.edu.co>
2023-09-19 18:42:18 -0500

    modificacion jwt

--- tallerApi/autenticacion/src/main/java/com/example/autenticacion/controllers/AutenticacionController.java
+++ tallerApi/autenticacion/src/main/java/com/example/autenticacion/controllers/AutenticacionController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/v1/autenticacion")
@@ -24,11 +27,24 @@ public class AutenticacionController {
      * @return
      * @throws Exception
      */
+
     @PostMapping("/login")
-    public ResponseEntity<String> login (@RequestBody String nombreUsuario, String password) throws Exception{
-        usuarioService.loginUsuario(nombreUsuario, password);
-        // retornar tokennnnn
-        return ResponseEntity.status(HttpStatus.OK).body("Resgistro Exitoso");
+    public ResponseEntity<String> login (@RequestBody String nombreUsuario, String password) throws Exception {
+        Usuario usuario = usuarioService.loginUsuario(nombreUsuario, password);
+        String token = AutenticacionController.generarTokenJWT(usuario);
+        return ResponseEntity.status(HttpStatus.OK).body(token);
+    }
+
+    public static String generarTokenJWT(Usuario usuario) {
+        String secretKey = "lUm27W{:{tVunMb6c()£WD1-rjl6H9Kkvci[+<?q"; // Use una clave de seguridad más segura en un entorno de producción.
+        long tiempoDeExpiracion = 1000 * 60 * 60 * 10; // 10 horas
+
+        return Jwts.builder()
+                .setSubject(usuario.getNombre())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + tiempoDeExpiracion))
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
     }
 
 }
