ac7ec98224453652abf6e0b7ad0fe68d5002b48d
danieleduardc <daniele.puertah@uqvirtual.edu.co>
2023-11-11 13:20:47 -0500

    implementacion la configuracion de docker en taller logs fase 2

--- tallerLogs/consumer.go
+++ tallerLogs/consumer.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %s", err)
 	}
--- tallerLogs/go.mod
+++ tallerLogs/go.mod
@@ -3,5 +3,3 @@ module consumer
 go 1.21
 
 require github.com/streadway/amqp v1.1.0
-
-require github.com/joho/godotenv v1.5.1 
